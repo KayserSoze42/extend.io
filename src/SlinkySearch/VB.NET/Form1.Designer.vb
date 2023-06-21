9c4251aa60d96fbfb59177aa59b50020573b8c00
KayserSoze42 <58406668+KayserSoze42@users.noreply.github.com>
2023-06-21 11:03:15 +0200

    A blessed commit
    An inefficient solution/attempt to clean up a Visual Studio solution. 
    
    I haven't touched neither Visual Studio nor VB/.NET for some time, yet this is another one of my "special" projects. 
    Working with this brought me back to my beginnings, making horrible GUIs in Visual Basic and despite having ugly design, it was surprisingly useful. 
    
    Rust In Pieces.

---
+++ src/SlinkySearch/VB.NET/Form1.Designer.vb
@@ -0,0 +1,222 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.okSearchButton = New System.Windows.Forms.Button()
+        Me.infoLabel = New System.Windows.Forms.Label()
+        Me.OpenButton = New System.Windows.Forms.Button()
+        Me.OpenFileDialog = New System.Windows.Forms.OpenFileDialog()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.FileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.OpenNewFileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ExitToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ExitToolStripMenuItem1 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ExitToolStripMenuItem2 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.EditToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SearchenterToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ResetFieldsCTRLRDelToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.AddNewElementToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.RemoveElements = New System.Windows.Forms.ToolStripMenuItem()
+        Me.RemoveAllElementsToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SaveFileDialog1 = New System.Windows.Forms.SaveFileDialog()
+        Me.NewButton = New System.Windows.Forms.Button()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'okSearchButton
+        '
+        Me.okSearchButton.BackColor = System.Drawing.SystemColors.GradientInactiveCaption
+        Me.okSearchButton.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.okSearchButton.ForeColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.okSearchButton.Location = New System.Drawing.Point(449, 124)
+        Me.okSearchButton.Name = "okSearchButton"
+        Me.okSearchButton.Size = New System.Drawing.Size(145, 32)
+        Me.okSearchButton.TabIndex = 0
+        Me.okSearchButton.Text = "OK"
+        Me.okSearchButton.UseVisualStyleBackColor = False
+        '
+        'infoLabel
+        '
+        Me.infoLabel.AutoSize = True
+        Me.infoLabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.infoLabel.Location = New System.Drawing.Point(146, 45)
+        Me.infoLabel.Name = "infoLabel"
+        Me.infoLabel.Size = New System.Drawing.Size(313, 50)
+        Me.infoLabel.TabIndex = 1
+        Me.infoLabel.Text = "Error reading the text file" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Open a file or a create new one"
+        Me.infoLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'OpenButton
+        '
+        Me.OpenButton.BackColor = System.Drawing.SystemColors.GradientInactiveCaption
+        Me.OpenButton.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.OpenButton.ForeColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.OpenButton.Location = New System.Drawing.Point(22, 124)
+        Me.OpenButton.Name = "OpenButton"
+        Me.OpenButton.Size = New System.Drawing.Size(145, 32)
+        Me.OpenButton.TabIndex = 2
+        Me.OpenButton.Text = "OPEN FILE"
+        Me.OpenButton.UseVisualStyleBackColor = False
+        '
+        'OpenFileDialog
+        '
+        Me.OpenFileDialog.FileName = "OpenFileDialog1"
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.BackColor = System.Drawing.Color.PowderBlue
+        Me.MenuStrip1.ImageScalingSize = New System.Drawing.Size(20, 20)
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileToolStripMenuItem, Me.EditToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(610, 28)
+        Me.MenuStrip1.TabIndex = 3
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'FileToolStripMenuItem
+        '
+        Me.FileToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.OpenNewFileToolStripMenuItem, Me.ExitToolStripMenuItem, Me.ExitToolStripMenuItem1, Me.ExitToolStripMenuItem2})
+        Me.FileToolStripMenuItem.Name = "FileToolStripMenuItem"
+        Me.FileToolStripMenuItem.Size = New System.Drawing.Size(46, 24)
+        Me.FileToolStripMenuItem.Text = "File"
+        '
+        'OpenNewFileToolStripMenuItem
+        '
+        Me.OpenNewFileToolStripMenuItem.Name = "OpenNewFileToolStripMenuItem"
+        Me.OpenNewFileToolStripMenuItem.Size = New System.Drawing.Size(202, 26)
+        Me.OpenNewFileToolStripMenuItem.Text = "Create New File"
+        '
+        'ExitToolStripMenuItem
+        '
+        Me.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
+        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(202, 26)
+        Me.ExitToolStripMenuItem.Text = "Open New File"
+        '
+        'ExitToolStripMenuItem1
+        '
+        Me.ExitToolStripMenuItem1.Name = "ExitToolStripMenuItem1"
+        Me.ExitToolStripMenuItem1.Size = New System.Drawing.Size(202, 26)
+        Me.ExitToolStripMenuItem1.Text = "Set Browser Path"
+        '
+        'ExitToolStripMenuItem2
+        '
+        Me.ExitToolStripMenuItem2.Name = "ExitToolStripMenuItem2"
+        Me.ExitToolStripMenuItem2.Size = New System.Drawing.Size(202, 26)
+        Me.ExitToolStripMenuItem2.Text = "Exit"
+        '
+        'EditToolStripMenuItem
+        '
+        Me.EditToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.SearchenterToolStripMenuItem, Me.ResetFieldsCTRLRDelToolStripMenuItem, Me.AddNewElementToolStripMenuItem, Me.RemoveElements})
+        Me.EditToolStripMenuItem.Name = "EditToolStripMenuItem"
+        Me.EditToolStripMenuItem.Size = New System.Drawing.Size(49, 24)
+        Me.EditToolStripMenuItem.Text = "Edit"
+        '
+        'SearchenterToolStripMenuItem
+        '
+        Me.SearchenterToolStripMenuItem.Name = "SearchenterToolStripMenuItem"
+        Me.SearchenterToolStripMenuItem.Size = New System.Drawing.Size(286, 26)
+        Me.SearchenterToolStripMenuItem.Text = "Search ( Enter )"
+        '
+        'ResetFieldsCTRLRDelToolStripMenuItem
+        '
+        Me.ResetFieldsCTRLRDelToolStripMenuItem.Name = "ResetFieldsCTRLRDelToolStripMenuItem"
+        Me.ResetFieldsCTRLRDelToolStripMenuItem.Size = New System.Drawing.Size(286, 26)
+        Me.ResetFieldsCTRLRDelToolStripMenuItem.Text = "Reset Fields ( Ctrl + R / del )"
+        '
+        'AddNewElementToolStripMenuItem
+        '
+        Me.AddNewElementToolStripMenuItem.Name = "AddNewElementToolStripMenuItem"
+        Me.AddNewElementToolStripMenuItem.Size = New System.Drawing.Size(286, 26)
+        Me.AddNewElementToolStripMenuItem.Text = "Add New Element ( Ctrl + N )"
+        '
+        'RemoveElements
+        '
+        Me.RemoveElements.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.RemoveAllElementsToolStripMenuItem})
+        Me.RemoveElements.Name = "RemoveElements"
+        Me.RemoveElements.Size = New System.Drawing.Size(286, 26)
+        Me.RemoveElements.Text = "Remove Elements"
+        '
+        'RemoveAllElementsToolStripMenuItem
+        '
+        Me.RemoveAllElementsToolStripMenuItem.Font = New System.Drawing.Font("Segoe UI Semibold", 9.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.RemoveAllElementsToolStripMenuItem.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.RemoveAllElementsToolStripMenuItem.Name = "RemoveAllElementsToolStripMenuItem"
+        Me.RemoveAllElementsToolStripMenuItem.Size = New System.Drawing.Size(233, 26)
+        Me.RemoveAllElementsToolStripMenuItem.Text = "Remove All Elements"
+        Me.RemoveAllElementsToolStripMenuItem.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'NewButton
+        '
+        Me.NewButton.BackColor = System.Drawing.SystemColors.GradientInactiveCaption
+        Me.NewButton.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.NewButton.ForeColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.NewButton.Location = New System.Drawing.Point(234, 124)
+        Me.NewButton.Name = "NewButton"
+        Me.NewButton.Size = New System.Drawing.Size(145, 32)
+        Me.NewButton.TabIndex = 4
+        Me.NewButton.Text = "NEW FILE"
+        Me.NewButton.UseVisualStyleBackColor = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.SkyBlue
+        Me.ClientSize = New System.Drawing.Size(610, 180)
+        Me.Controls.Add(Me.NewButton)
+        Me.Controls.Add(Me.OpenButton)
+        Me.Controls.Add(Me.infoLabel)
+        Me.Controls.Add(Me.okSearchButton)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.KeyPreview = True
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "Form1"
+        Me.Text = "SLinky - CLI"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents okSearchButton As Button
+    Friend WithEvents infoLabel As Label
+    Friend WithEvents OpenButton As Button
+    Friend WithEvents OpenFileDialog As OpenFileDialog
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents FileToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents OpenNewFileToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ExitToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ExitToolStripMenuItem1 As ToolStripMenuItem
+    Friend WithEvents ExitToolStripMenuItem2 As ToolStripMenuItem
+    Friend WithEvents SaveFileDialog1 As SaveFileDialog
+    Friend WithEvents NewButton As Button
+    Friend WithEvents EditToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents SearchenterToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ResetFieldsCTRLRDelToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents AddNewElementToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents RemoveElements As ToolStripMenuItem
+    Friend WithEvents RemoveAllElementsToolStripMenuItem As ToolStripMenuItem
+End Class
---
+++ src/SlinkySearch/VB.NET/Form1.vb
@@ -0,0 +1,453 @@
+﻿Public Class Form1
+
+    Dim objReader As System.IO.StreamReader
+    Dim objWriter As System.IO.StreamWriter
+
+    Dim links As New List(Of String())()
+
+    Private Function ReLoad(sender As Object, e As EventArgs)
+
+        links.Clear()
+        Me.Controls.Clear()
+        InitializeComponent()
+        Form1_Load(e, e)
+
+        If My.Settings.filePath = "" Then
+
+            Me.Width = 474
+            Me.Height = 185
+
+            For i As Integer = Me.Controls.Count - 1 To 0 Step -1
+                If TypeOf Me.Controls(i) Is TextBox Then
+                    Dim textBoxControl As TextBox = TryCast(Me.Controls(i), TextBox)
+                    If textBoxControl IsNot Nothing Then
+                        textBoxControl.Enabled = False
+                    End If
+                    Me.Controls.RemoveAt(i)
+                ElseIf TypeOf Me.Controls(i) Is Label And Me.Controls(i).Name <> infoLabel.Name Then
+                    Me.Controls.RemoveAt(i)
+                End If
+            Next
+
+            okSearchButton.Location = New Point(337, 101)
+            OpenButton.Location = New Point(16, 101)
+
+        End If
+
+    End Function
+
+    Private Function AddElement()
+
+        If My.Settings.filePath = "" Then
+            Return 0
+        End If
+
+        objWriter = New System.IO.StreamWriter(My.Settings.filePath, True)
+
+        Dim newName, newUrlStart, newUrlEnd
+
+        newName = InputBox("Enter the name of the new search", "New element")
+
+        If newName <> "" Then
+
+            newUrlStart = InputBox("Enter the start of the Url search pattern", "New element")
+
+            If newUrlStart <> "" Then
+
+                newUrlEnd = InputBox("Enter the end of the Url search pattern " & vbCrLf & " (optional, if N/A just leave empty)", "New element")
+
+                If newUrlEnd <> "" Then
+                    objWriter.WriteLine(newName + " <<>> " + newUrlStart + " <<>> " + newUrlEnd)
+                Else
+                    objWriter.WriteLine(newName + " <<>> " + newUrlStart)
+                End If
+
+
+
+            End If
+
+        End If
+
+        objWriter.Close()
+
+        Return 1
+
+    End Function
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        If Not IO.File.Exists(My.Settings.filePath) And My.Settings.filePath <> "" Then
+
+            My.Settings.filePath = ""
+            My.Settings.Save()
+
+            ReLoad(e, e)
+
+        End If
+
+
+
+        Dim TextLine As String
+
+        If My.Settings.filePath = "" Then
+
+        Else
+
+            OpenButton.Visible = False
+            OpenButton.Enabled = False
+
+            NewButton.Visible = False
+            NewButton.Enabled = False
+
+            infoLabel.Text = ""
+            okSearchButton.Text = "Search"
+
+            Dim yPos As Integer
+            yPos = 0
+
+            Dim textFile As List(Of String) = System.IO.File.ReadAllLines(My.Settings.filePath).ToList
+
+            Dim lineCount As Integer = 0
+
+            For Each line As String In textFile
+
+                lineCount += 1
+
+                If line = "" Then
+                    textFile.RemoveAt(lineCount)
+                    lineCount -= 1
+                End If
+
+            Next
+
+            System.IO.File.WriteAllLines(My.Settings.filePath, textFile)
+
+            If textFile.Count = 0 Then
+
+                My.Settings.filePath = ""
+                My.Settings.Save()
+
+                ReLoad(e, e)
+
+                Return
+
+            End If
+
+            objReader = New System.IO.StreamReader(My.Settings.filePath)
+
+            Me.Width = 250
+
+            Dim arraySplitString As String()
+
+            Dim rowNo As Integer
+
+            Dim newArray() As String = {}
+
+            Do While objReader.Peek() <> -1
+
+                yPos += 50
+
+                TextLine = objReader.ReadLine()
+
+                If TextLine = "" Then
+                    Continue Do
+                End If
+
+                arraySplitString = Split(TextLine, " <<>> ")
+
+                If arraySplitString.Length < 2 Then
+
+                    Dim message As String
+                    Dim infoDialog
+
+                    rowNo = yPos / 50
+                    message = "Task failed successfully! Error on row " + rowNo.ToString()
+
+                    infoDialog = MsgBox(message, MsgBoxStyle.OkOnly + MsgBoxStyle.Information, "Failed to read")
+
+                    If infoDialog = vbOK Then
+
+                        My.Settings.filePath = ""
+                        My.Settings.Save()
+
+                        ReLoad(e, e)
+
+                    End If
+
+                End If
+
+                If arraySplitString.Length = 3 Then
+                    newArray = {arraySplitString(1).Replace(" ", ""), arraySplitString(2).Replace(" ", "")}
+                ElseIf arraySplitString.Length = 2 Then
+                    newArray = {arraySplitString(1), New String("")}
+                End If
+
+                links.Add(newArray)
+
+                okSearchButton.Location = New Point(50, yPos + 50)
+
+                Me.Height += 40
+
+                Dim newTextbox As New TextBox
+                With newTextbox
+                    .Size = New Size(190, 20)
+                    .Location = New Point(20, yPos)
+                End With
+                Me.Controls.Add(newTextbox)
+
+                Dim newLabel As New Label
+                With newLabel
+                    .Size = New Size(390, 30)
+                    .Location = New Point(30, yPos - 20)
+                    .Text = arraySplitString(0)
+                    .Font = New Font("Comic Sans MS", 9, FontStyle.Bold)
+                End With
+                Me.Controls.Add(newLabel)
+
+                Me.MainMenuStrip = MenuStrip1
+
+                Dim newElement As New ToolStripMenuItem()
+
+                rowNo = (yPos / 50) - 1
+
+                With newElement
+                    .Name = "item" + rowNo.ToString()
+                    .Text = arraySplitString(0)
+                    .Font = New Font("Segoe UI Semibold", 9, FontStyle.Italic)
+                End With
+
+                RemoveElements.DropDownItems.Add(newElement)
+                AddHandler newElement.Click, AddressOf RemoveAllElementsToolStripMenuItem_Click
+
+                newArray = {}
+                arraySplitString = {}
+
+            Loop
+
+            objReader.Close()
+
+        End If
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles okSearchButton.Click
+
+        If My.Settings.filePath = "" Then
+
+            Me.Close()
+
+        Else
+
+            Dim count As Integer = 0
+
+            For Each c As TextBox In Me.Controls.OfType(Of TextBox)
+
+                If c.Text <> "" And c.Enabled = True Then
+
+                    Dim currentLink As String = links(count)(0) + c.Text.Replace(" ", "") + links(count)(1)
+
+                    Try
+                        Process.Start(My.Settings.browserPath, currentLink)
+                    Catch ex As Exception
+                        Dim infoBox
+                        infoBox = MsgBox("Task failed successfully! Error with link / browser path", vbOKOnly, "Failed")
+
+                        My.Settings.browserPath = "C:\Program Files\Mozilla Firefox\firefox.exe"
+                        My.Settings.Save()
+                    End Try
+
+
+
+                End If
+
+                count += 1
+
+            Next
+
+        End If
+
+    End Sub
+
+    Private Sub OpenButton_Click_1(sender As Object, e As EventArgs) Handles OpenButton.Click
+
+        OpenFileDialog.ShowDialog()
+
+        My.Settings.filePath = OpenFileDialog.FileName
+        My.Settings.Save()
+
+        ReLoad(e, e)
+
+    End Sub
+
+    Private Sub ExitToolStripMenuItem1_Click(sender As Object, e As EventArgs) Handles ExitToolStripMenuItem1.Click
+
+        Dim inputDialog
+        inputDialog = InputBox("Enter Browser Path For Opening Links", "Path Change", My.Settings.browserPath)
+
+        If inputDialog <> "" Then
+
+            My.Settings.browserPath = inputDialog
+
+        End If
+
+    End Sub
+
+    Private Sub ExitToolStripMenuItem2_Click(sender As Object, e As EventArgs) Handles ExitToolStripMenuItem2.Click
+        Me.Close()
+
+    End Sub
+
+    Private Sub ExitToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ExitToolStripMenuItem.Click
+        OpenButton_Click_1(e, e)
+
+    End Sub
+
+    Private Sub ResetFieldsCTRLRDelToolStripMenuItem_Click(sender As Object, e As EventArgs)
+        For i As Integer = Me.Controls.Count - 1 To 0 Step -1
+            If TypeOf Me.Controls(i) Is TextBox Then
+                Me.Controls(i).Text = ""
+            End If
+        Next
+    End Sub
+
+    Private Sub Form1_KeyDown(sender As Object, e As KeyEventArgs) Handles Me.KeyDown
+        If (e.Control AndAlso e.KeyCode = Keys.R) Or e.KeyCode = Keys.Delete Then
+
+            For i As Integer = Me.Controls.Count - 1 To 0 Step -1
+                If TypeOf Me.Controls(i) Is TextBox Then
+                    Me.Controls(i).Text = ""
+                End If
+            Next
+
+        ElseIf e.KeyCode = Keys.Enter Then
+
+            okSearchButton.PerformClick()
+
+        End If
+    End Sub
+
+    Private Sub OpenNewFileToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles OpenNewFileToolStripMenuItem.Click
+
+        NewButton.PerformClick()
+
+    End Sub
+
+    Private Sub NewButton_Click(sender As Object, e As EventArgs) Handles NewButton.Click
+
+        SaveFileDialog1.Filter = "TXT Files (*.txt*)|*.txt"
+
+        If SaveFileDialog1.ShowDialog = Windows.Forms.DialogResult.OK Then
+
+            My.Settings.filePath = SaveFileDialog1.FileName
+            My.Settings.Save()
+
+            Dim shouldAdd As Boolean
+            shouldAdd = True
+
+            Dim infoBox, addMoreBox
+            infoBox = MsgBox("Enter the first element for the new file", vbOKOnly, "Init")
+
+            While shouldAdd
+
+                AddElement()
+
+                addMoreBox = MsgBox("Do you want to add another element now?", vbYesNo, "Init")
+
+                If addMoreBox = vbNo Then
+                    shouldAdd = False
+                End If
+
+            End While
+
+            ReLoad(e, e)
+        End If
+
+    End Sub
+
+    Private Sub AddNewElementToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles AddNewElementToolStripMenuItem.Click
+
+        Dim shouldAdd As Boolean
+        shouldAdd = True
+
+        Dim addMoreBox, returnValue
+
+        While shouldAdd
+
+            returnValue = AddElement()
+
+            If returnValue = 0 Then
+                shouldAdd = False
+                Exit While
+            End If
+
+            addMoreBox = MsgBox("Do you want to add another element now?", vbYesNo, "Init")
+
+            If addMoreBox = vbNo Then
+                shouldAdd = False
+            End If
+
+        End While
+
+        ReLoad(e, e)
+
+    End Sub
+
+    Private Sub RemoveElements_Click(sender As Object, e As EventArgs) Handles RemoveElements.Click
+
+    End Sub
+
+    Private Sub RemoveAllElementsToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles RemoveAllElementsToolStripMenuItem.Click
+
+        Dim clickedItem As ToolStripMenuItem = TryCast(sender, ToolStripMenuItem)
+
+        If clickedItem IsNot Nothing Then
+
+            If clickedItem.Name = "RemoveAllElementsToolStripMenuItem" Then
+
+                Dim confirmationBox, infoBox
+
+                confirmationBox = MsgBox("Remove all elements?", vbYesNo, "Confirm")
+
+                If confirmationBox = vbYes Then
+
+                    infoBox = MsgBox("Enter new first element", vbOKOnly, "reInit")
+
+                    System.IO.File.WriteAllText(My.Settings.filePath, "")
+
+                    Dim shouldAdd As Boolean
+                    shouldAdd = True
+
+                    Dim addMoreBox
+
+                    While shouldAdd
+
+                        AddElement()
+
+                        addMoreBox = MsgBox("Do you want to add another element now?", vbYesNo, "Init")
+
+                        If addMoreBox = vbNo Then
+                            shouldAdd = False
+                        End If
+
+                    End While
+
+                    ReLoad(e, e)
+
+                End If
+
+            Else
+
+                Dim fileLines As List(Of String) = System.IO.File.ReadAllLines(My.Settings.filePath).ToList
+
+                fileLines.RemoveAt(Convert.ToInt32(Convert.ToDecimal(Mid(clickedItem.Name, 5))))
+
+                System.IO.File.WriteAllLines(My.Settings.filePath, fileLines)
+
+                ReLoad(e, e)
+
+            End If
+
+        End If
+
+    End Sub
+End Class
