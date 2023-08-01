a998b5306114e924b5d15888b370719d70d5417a
KayserSoze42 <morus.hana@gmail.com>
2023-08-01 15:54:47 +0200

    now curried
    but not tested

--- src/JScript/persona/linkelol.js
+++ src/JScript/persona/linkelol.js
@@ -1,16 +1,25 @@
 
-function add(a, b) {
-
-	return a+b;
-
-}
+function add(a, b) {return a+b;}
 
 const result = add.call(null, 5, 10);
-console.log(result);
 
+// -------------------------------------
 
 const addAgain = (a,b) => a+b;
 
-const addTwo = addAgain.call(null, 5, 10);
+const resultAgain = addAgain.call(null, 5, 10);
 
-console.log(addTwo);
+// -------------------------------------
+
+const addTwoForm = (what, a, b) => add.call(what, a, b);
+
+const addTwoCurry = a =>
+	b=>
+		addTwoForm(null, a, b);
+
+const resultOnceAgain = addTwoCurry(5)(10);
+
+console.log(result);
+console.log(resultAgain);
+console.log(resultOnceAgain);
+
